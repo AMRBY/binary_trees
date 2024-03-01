674f50ad536d8f1ad8694b022d1128b92deeae95
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-01 23:30:33 +0000

    mandatory

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,10 +9,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (tree->left && tree->right)
-		return (2);
-	else if (tree->left || tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
 	else
-		return (1);
+		return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
 }
