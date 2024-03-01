97bdb9b0a97c779d56591e03fe667a4f315c8706
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-01 22:57:02 +0000

    mandatory

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+* binary_tree_leaves - Binary tree node
+* Description: insert a node
+* Return: void
+* @tree: all the tree
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t r = 0, l = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left && tree->right)
+		return (2);
+	if else (tree->left || tree->right)
+		return (1);
+	else 
+		return (0);
+}
