7fc97b19ebc71f182f1973c5867d8cc19eda6939
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-03 17:53:49 +0000

    mandatory

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,27 @@
 #include "binary_trees.h"
 /**
+* binary_tree_height - Binary tree node
+* Description: insert a node
+* Return: void
+* @tree: all the tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t hr = 0, hl = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+	{
+		hl = binary_tree_height(tree->left) + 1;
+	}
+	if (tree->right)
+	{
+		hr = binary_tree_height(tree->right) + 1;
+	}
+	return (hl >= hr ? hl : hr);
+}
+/**
 * binary_tree_balance - Binary tree node
 * Description: insert a node
 * Return: void
