a112b1b39a3059eb867b5f3c00978e1323499197
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-05 00:05:05 +0000

    mandatory

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,6 +10,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
 	if (tree->right && tree->left)
 	{
 		if (binary_tree_height(tree->right) == binary_tree_height(tree->left))
